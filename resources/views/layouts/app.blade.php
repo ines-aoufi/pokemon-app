8f898f04de0983798c48677fe78d554ca7b94d99
iaoufisys <inesaoufii@gmail.com>
2026-03-09 16:47:47 +0100

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Pokemon App') }}</title>
+    <title>Pokemon - Inès</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
